d3cdc97fde9b5c03681887b7a610398a4bdc2b39
AN1001 <arnav.nagpure@icloud.com>
2024-07-14 21:16:02 +0100

    Deployed Website and added multiple sections

--- JS/src/App.tsx
+++ JS/src/App.tsx
@@ -1,20 +1,29 @@
-import ListGroup from "./ListGroup";
 import Navbar from "./Navbar";
 import "./App.css";
-
-function onSelectItem(event: React.BaseSyntheticEvent) {
-  console.log(event.target);
-}
+import { useState } from "react";
 
 function App() {
+  const [activePage, setActivePage] = useState("1-2");
+
+  const renderPage = () => {
+    switch (activePage) {
+      case "1-2":
+        return <h1>Task 1-2</h1>;
+      case "3":
+        return <h1>Task 3</h1>;
+      case "4-7":
+        return <h1>Tasks 4-7</h1>;
+      case "Extensions":
+        return <h1>Extensions</h1>;
+      default:
+        return <h1>Error</h1>;
+    }
+  };
+
   return (
     <div>
-      <Navbar></Navbar>
-      <ListGroup
-        items={["Uno", "Dos", "Tres", "Quatro"]}
-        heading="Numbers"
-        onSelectItem={onSelectItem}
-      ></ListGroup>
+      <Navbar changeHome={setActivePage} activeHeader={activePage}></Navbar>
+      {renderPage()}
     </div>
   );
 }
